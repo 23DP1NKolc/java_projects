c8f1d2e4b72073cd28450d1ebda57584e9af9d4f
Nikita Kolcovs <A230821NK@rvt.lv>
2025-03-05 09:46:45 +0200

    timer v0

---
+++ test.py
@@ -0,0 +1,12 @@
+import time
+countdown_time= 60
+while countdown_time >0:
+    print('\033[F\033[K', end='')
+    print(countdown_time)
+    time.sleep(1)
+    countdown_time -=1
+while countdown_time==1:
+    print('VENOOOOOOOOOOOOOOOOOOOOOOOOM')
+
+    
+
